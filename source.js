9e2b72ed1f79945d4c6d7c2e6598bb79c32b3959
Samuca <35107922+Samuuuca@users.noreply.github.com>
2022-10-09 11:53:19 -0300

    Update source.js

--- source.js
+++ source.js
@@ -1,5 +1,11 @@
-
+getUserPosition();
 date();
+
+const Hora = new Date();
+
+const result = Hora.getHours();
+
+
 function date(){
 
     const dateBD = document.getElementById('dateBD')
@@ -38,3 +44,64 @@ function carossel(e) {
         
     }
 }
+
+function getUserPosition() {
+  let url;
+  navigator.geolocation.getCurrentPosition((pos) => {
+    let lat = pos.coords.latitude;
+    let long = pos.coords.longitude;
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=272d5dbf2036bbd82b9242d7f1d82023`;
+    fetchApi(url);
+  });
+}
+
+function fetchApi(url) {
+    let city = document.querySelector('.city');
+    let temp = document.querySelector('span');
+    let ImgClimate = document.getElementById('climateImg');
+    console.log(ImgClimate);
+    
+    
+    fetch(url).then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+      let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
+      
+      if( result >= 7 && result <= 18){
+        if (tempInCelsius >= 20) {
+          
+          ImgClimate.src = 'img/cliamte/sol-quente.png'
+          console.log('quente');
+        }
+        else if (tempInCelsius >= 13 && tempInCelsius < 20 ) {
+          
+          ImgClimate.src = 'img/cliamte/frio-dia.png'
+        } else {
+          ImgClimate.src = 'img/cliamte/neve.png'
+        }
+      }
+      else{
+        if (tempInCelsius >= 20) {
+          
+          ImgClimate.src = 'img/cliamte/lua-quente.png'
+          console.log('quente');
+        }
+        else if (tempInCelsius >= 13 && tempInCelsius < 20 ) {
+          
+          ImgClimate.src = 'img/cliamte/lua-frio.png'
+        } else {
+          ImgClimate.src = 'img/cliamte/neve.png'
+        }
+        console.log('noite')
+      }
+
+      city.innerText = `${data.name} ${tempInCelsius}°C`;
+      
+    })
+    .catch((err) => {
+      city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
+      temp.innerText = `-`;
+    })
+  }
+  
